54da040e229498caaed9184fd746c4e04e1786d8
Lightlawn <lightlawn@gmail.com>
2021-09-11 19:57:51 +0200

    adds feedback jsx file

---
+++ src/components/feedback.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Feedback extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+    }
+    
+    render() {
+        return 
+    }
+}
